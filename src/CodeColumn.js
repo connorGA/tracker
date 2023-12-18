3dc9626bdf7fd127ffc091b5d0ffa628dfa4cd50
connorGA <ronnoccap@gmail.com>
2023-12-17 18:20:15 -0800

    code column bar graph working

--- src/CodeColumn.js
+++ src/CodeColumn.js
@@ -1,49 +1,91 @@
 import React, { useState } from 'react';
 
 const TimeInput = ({ onSubmit }) => {
-    const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState('');
 
-    const handleInputChange = (e) => {
-        setInputValue(e.target.value);
-    };
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onSubmit(inputValue);
-        setInputValue('');
-      };
-      return (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            placeholder="Enter time (e.g., 1 hour, 2 hours 15 mins)"
-            value={inputValue}
-            onChange={handleInputChange}
-          />
-          <button type="submit">Save</button>
-        </form>
-      );
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(inputValue);
+    setInputValue('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter time (e.g., 1 hour, 2 hours 15 mins)"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <button type="submit">Save</button>
+    </form>
+  );
+};
 
 export const CodeColumn = () => {
-    const [times, setTimes] = useState([]);
+  const [times, setTimes] = useState([]);
+  const [hoveredTime, setHoveredTime] = useState(null);
 
-    const handleTimeSubmit = (time) => {
-      setTimes([...times, time]);
-    };
-  
-    return (
+  const handleTimeSubmit = (time) => {
+    setTimes([...times, time]);
+  };
+
+  // Calculate the total hours for styling the bar graph
+  const totalHours = times.reduce((acc, time) => acc + parseFloat(time), 0);
+
+  // Total hours you want to reach
+  const targetTotalHours = 730;
+
+  return (
+    <div>
+      <h2>Code</h2>
+      <TimeInput onSubmit={handleTimeSubmit} />
       <div>
-        <h2>Code</h2>
-        <TimeInput onSubmit={handleTimeSubmit} />
-        <div>
-          <strong>Hours:</strong>
-          <ul>
-            {times.map((time, i) => (
-              <li key={i}>{time}</li>
-            ))}
-          </ul>
+        <strong>Hours:</strong>
+        <div
+          style={{
+            display: 'flex',
+            height: '40px',
+            backgroundColor: 'black',
+          }}
+        >
+          {times.map((time, i) => (
+            <div
+              key={i}
+              style={{
+                backgroundColor: '#4caf50', // Green color
+                width: `${(parseFloat(time) / targetTotalHours) * 100}%`,
+                marginRight: '2px', // Space between bars
+                position: 'relative',
+              }}
+              onMouseEnter={() => setHoveredTime(time)}
+              onMouseLeave={() => setHoveredTime(null)}
+            >
+              {hoveredTime === time && (
+                <div
+                  style={{
+                    position: 'absolute',
+                    top: '-20px',
+                    left: '50%',
+                    transform: 'translateX(-50%)',
+                    backgroundColor: '#333',
+                    color: '#fff',
+                    padding: '5px',
+                    borderRadius: '3px',
+                  }}
+                >
+                  {time}
+                </div>
+              )}
+            </div>
+          ))}
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
+
