a2a212e31dee0da559a98ba9400a2a58303852c3
connorGA <ronnoccap@gmail.com>
2024-01-11 18:39:43 -0800

    Mongodb cluster created on Atlas and connected to application

--- server/server.js
+++ server/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
+const mongoose = require('mongoose');
 const port = process.env.PORT || 5000;
+
+// database
+mongoose
+.connect(process.env.MONGO_URL)
+.then(() => console.log("DB connected"))
+.catch((err) => console.log("DB NOT connected"))
 
 
 app.get('/', (req,res) => {
