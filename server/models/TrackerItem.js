dc200681240ee8df30fdfdff91660a41cfa389ae
connorGA <ronnoccap@gmail.com>
2024-01-14 21:43:30 -0800

    add trackeritem schema

---
+++ server/models/TrackerItem.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const trackerItemSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    hoursCollected: { type: Number, default: 0 },
+    goalHours: { type: Number, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+  });
+  
+  const TrackerItem = mongoose.model('TrackerItem', trackerItemSchema);
+  
